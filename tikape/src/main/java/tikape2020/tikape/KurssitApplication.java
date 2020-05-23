0822006ee5440eb9cf5514f2b98d4d4fe23c214c
jahlgren <jukka.ahlgren@gmail.com>
2020-05-23 23:51:21 +0300

    Initialize application

---
+++ tikape/src/main/java/tikape2020/tikape/KurssitApplication.java
@@ -0,0 +1,15 @@
+package tikape2020.tikape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KurssitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KurssitApplication.class, args);
+        System.out.println("KurssitApplication.class");
+
+    }
+
+}
---
+++ tikape/src/test/java/tikape2020/tikape/KurssitApplicationTests.java
@@ -0,0 +1,13 @@
+package tikape2020.tikape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KurssitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
